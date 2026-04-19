93be8e7b6ed67fa1c3ea4b886f067a2a4c6221de
Benjamin Pelletier <pelletierb@wing.com>
2026-04-19 01:14:16 +0000

    Fix basedpyright misinterpretation

--- src/implicitdict/__init__.py
+++ src/implicitdict/__init__.py
@@ -356,10 +356,10 @@ class StringBasedTimeDelta(str):
             reformat: If true, override a provided string with a string representation of the parsed timedelta.
         """
         if isinstance(value, str):
-            seconds = pytimeparse2.parse(value)
-            if seconds is None:
+            dt = pytimeparse2.parse(value, as_timedelta=True)
+            if dt is None:
                 raise ValueError(f"Could not parse type {type(value).__name__} into StringBasedTimeDelta")
-            dt = datetime.timedelta(seconds=seconds)
+            assert isinstance(dt, datetime.timedelta)
             s = str(dt) if reformat else value
         elif isinstance(value, float) or isinstance(value, int):
             dt = datetime.timedelta(seconds=value)
